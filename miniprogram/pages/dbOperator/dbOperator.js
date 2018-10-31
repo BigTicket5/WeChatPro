4edd8c7037efcfb2dee3f578deb1e8bec7f6684d
Zhenzhong Mao <dkrose.random@gmail.com>
2018-10-31 15:50:33 +0800

    20181031

--- miniprogram/pages/dbOperator/dbOperator.js
+++ miniprogram/pages/dbOperator/dbOperator.js
@@ -22,36 +22,25 @@ Page({
     this.onQuery();
   },
   onQuery: function() {
-    const db = wx.cloud.database()
-    // 查询所有模拟题
-    db.collection('salesforcedevexamlib').get({
-      success: res => {
+    wx.cloud.callFunction({
+      name:'getAdminExamQuestions',
+      complete:res=>{
         this.setData({
-          queryResult: res.data
+          queryResult:res.result.data
         })
-        console.log('[数据库] [查询记录] 成功: ', res.data);
-      },
-      fail: err => {
-        wx.showToast({
-          icon: 'none',
-          title: '查询记录失败'
-        })
-        console.error('[数据库] [查询记录] 失败：', err)
       }
     })
   },
 
   checkboxChange:function(e){
-    console.log('checkBox发生change事件，携带value值为：', e.detail.value);
     var tmp = [];
     tmp=e.detail.value;
     this.setData({
       choosenIndex : tmp
     });
-    console.log(this.data.choosenIndex);
   },
+  //get correct answer to this question
   commitAnswer: function(){
-    console.log(this.data.choosenIndex);
     var idx = this.data.counterId;
     var answerArray = [];
     answerArray = this.data.queryResult[idx].question_answer;
@@ -59,17 +48,14 @@ Page({
     cIdx=this.data.choosenIndex;
     var ansO =[]
     for(var i=0;i<answerArray.length;i++){
-      console.log(app.data.optionNames[answerArray[i]]);
       ansO.push(app.data.optionNames[answerArray[i]-1]);
     }
     this.setData({
       ansOption: ansO 
     });
-    console.log(this.data.ansOption);
     for(var i=0;i<cIdx.length;i++){
       var choosenIDX = parseInt(cIdx[i]) + 1;
       var bc = "backcolor["+cIdx[i]+"]";
-      console.log(bc);
       if(answerArray.indexOf(choosenIDX)>=0){
         this.setData({
           [bc]:'green'
@@ -90,8 +76,8 @@ Page({
     })
   },
  
+  // go to next question
   nextQuestion:function(){
-    console.log(this.data.counterId);
     this.setData({
       choosenBox:[],
       counterId:this.data.counterId+1,
@@ -105,6 +91,7 @@ Page({
     });
   },
   
+  // go to prev question
   preQuestion:function(){
     if(this.data.counterId>0){
       this.setData({
@@ -119,46 +106,6 @@ Page({
         }
       });
     }
-  },
-
-  nextStep: function () {
-    // 在第一步，需检查是否有 openid，如无需获取
-    if (this.data.step === 1 && !this.data.openid) {
-      wx.cloud.callFunction({
-        name: 'login',
-        data: {},
-        success: res => {
-          app.globalData.openid = res.result.openid
-          this.setData({
-            step: 2,
-            openid: res.result.openid
-          })
-        },
-        fail: err => {
-          wx.showToast({
-            icon: 'none',
-            title: '获取 openid 失败，请检查是否有部署 login 云函数',
-          })
-          console.log('[云函数] [login] 获取 openid 失败，请检查是否有部署云函数，错误信息：', err)
-        }
-      })
-    } else {
-      const callback = this.data.step !== 6 ? function() {} : function() {
-        console.group('数据库文档')
-        console.log('https://developers.weixin.qq.com/miniprogram/dev/wxcloud/guide/database.html')
-        console.groupEnd()
-      }
-
-      this.setData({
-        step: this.data.step + 1
-      }, callback)
-    }
-  },
-
-  prevStep: function () {
-    this.setData({
-      step: this.data.step - 1
-    })
   },
   goHome: function() {
     const pages = getCurrentPages()
