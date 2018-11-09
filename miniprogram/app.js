4542d37a4cfc18239c9dde8831df4b9d2743818a
Zhenzhong Mao <dkrose.random@gmail.com>
2018-11-09 17:29:41 +0800

    20181109_2

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,45 +1,63 @@
 //app.js
 App({
   onLaunch: function () {
+    wx.cloud.init();
     var that = this
     var user=wx.getStorageSync('user') || {};  
     var userInfo=wx.getStorageSync('userInfo') || {}; 
-    if((!user.openid || (user.expires_in || Date.now()) < (Date.now() + 600))&&(!userInfo.nickName)){ 
-       wx.login({  
-       success: function(res){ 
-           if(res.code) {
-               wx.getUserInfo({
-                   success: function (res) {
-                       var objz={};
-                       objz.avatarUrl=res.userInfo.avatarUrl;
-                       objz.nickName=res.userInfo.nickName;
-                       //console.log(objz);
-                       wx.setStorageSync('userInfo', objz);//存储userInfo
-                   }
-               });
-               var d=that.globalData;//这里存储了appid、secret、token串  
-               var l='https://api.weixin.qq.com/sns/jscode2session?appid='+d.appid+'&secret='+d.secret+'&js_code='+res.code+'&grant_type=authorization_code';  
-               wx.request({  
-                   url: l,  
-                   data: {},  
-                   method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT  
-                   // header: {}, // 设置请求的 header  
-                   success: function(res){ 
-                       var obj={};
-                       obj.openid=res.data.openid;  
-                       obj.expires_in=Date.now()+res.data.expires_in;  
-                       //console.log(obj);
-                       wx.setStorageSync('user', obj);//存储openid  
-                   }  
-               });
-           }else {
-               console.log('获取用户登录态失败！' + res.errMsg)
-           }          
-       }  
-     }); 
-   } 
+    userInfo={};
+    if((!user.openid || (user.expires_in || Date.now()) < (Date.now() + 600))&&(!userInfo.nickName)){
+        wx.showModal({
+            title: '微信授权', 
+            content: 'ADM201题库申请获得以下权限:',  
+              success: function(res) {  
+                  if (res.confirm) {
+                    wx.login({  
+                    success: function(res){ 
+                        if(res.code) {
+                            wx.getUserInfo({
+                                success: function (res) {
+                                    var objz={};
+                                    objz.avatarUrl=res.userInfo.avatarUrl;
+                                    objz.nickName=res.userInfo.nickName;
+                                    wx.setStorageSync('userInfo', objz);//存储userInfo
+                                }
+                            });
+                            var d=that.globalData;//这里存储了appid、secret、token串  
+                            var l='https://api.weixin.qq.com/sns/jscode2session?appid='+d.appid+'&secret='+d.secret+'&js_code='+res.code+'&grant_type=authorization_code';  
+                            wx.request({  
+                                url: l,  
+                                data: {},  
+                                method: 'GET',
+                                success: function(res){ 
+                                    var obj={};
+                                    obj.openid=res.data.openid;  
+                                    obj.expires_in=Date.now()+res.data.expires_in;  
+                                    //console.log(obj);
+                                    wx.setStorageSync('user', obj);//存储openid  
+                                }  
+                            });
+                        }else {
+                            console.log('获取用户登录态失败！' + res.errMsg)
+                        }          
+                    }  
+                    });
+                }else if (res.cancel) {  
+                    console.log('用户拒绝');  
+                }  
+            }
+        }) 
+    }
+    else{
+        console.log(userInfo);
+    }
   },
   data:{
     optionNames:['A','B','C','D','E','F']
+  },
+  globalData:{
+    optionNames:['A','B','C','D','E','F'],
+    appid:'',
+    secret:''
   }
 })
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -2,49 +2,6 @@
 const app = getApp()
 
 Page({
-  onLoad: function() {
-    // 查看是否授权
-    wx.getSetting({
-      success (res){
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
-          wx.getUserInfo({
-            success: function(res) {
-              console.log(res.userInfo)
-            }
-          }),
-          wx.login({
-            success (res) {
-              if (res.code) {
-                //发起网络请求
-                wx.request({
-                  url: 'https://test.com/onLogin',
-                  data: {
-                    code: res.code
-                  }
-                })
-              } else {
-                console.log('登录失败！' + res.errMsg)
-              }
-            }
-          })
-        }
-        else{
-          wx.showModal({
-            title: '微信授权', 
-            content: 'ADM201题库申请获得以下权限:',  
-              success: function(res) {  
-                  if (res.confirm) {  
-                    console.log('用户同意')
-                  } else if (res.cancel) {  
-                    console.log('用户拒绝')  
-                  }  
-              }  
-          })
-        }
-      }
-    })
-  },
   startAns:function(){
     wx.navigateTo({
       url: '../dbOperator/dbOperator'
