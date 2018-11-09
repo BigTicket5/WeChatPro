558745762fdb8bfe26852bbb4fce26cff7799eaa
Zhenzhong Mao <dkrose.random@gmail.com>
2018-11-09 10:56:18 +0800

    20181109

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,16 +1,43 @@
 //app.js
 App({
   onLaunch: function () {
-    
-    if (!wx.cloud) {
-      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
-    } else {
-      wx.cloud.init({
-        traceUser: true,
-      })
-    }
-
-    this.globalData = {}
+    var that = this
+    var user=wx.getStorageSync('user') || {};  
+    var userInfo=wx.getStorageSync('userInfo') || {}; 
+    if((!user.openid || (user.expires_in || Date.now()) < (Date.now() + 600))&&(!userInfo.nickName)){ 
+       wx.login({  
+       success: function(res){ 
+           if(res.code) {
+               wx.getUserInfo({
+                   success: function (res) {
+                       var objz={};
+                       objz.avatarUrl=res.userInfo.avatarUrl;
+                       objz.nickName=res.userInfo.nickName;
+                       //console.log(objz);
+                       wx.setStorageSync('userInfo', objz);//存储userInfo
+                   }
+               });
+               var d=that.globalData;//这里存储了appid、secret、token串  
+               var l='https://api.weixin.qq.com/sns/jscode2session?appid='+d.appid+'&secret='+d.secret+'&js_code='+res.code+'&grant_type=authorization_code';  
+               wx.request({  
+                   url: l,  
+                   data: {},  
+                   method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT  
+                   // header: {}, // 设置请求的 header  
+                   success: function(res){ 
+                       var obj={};
+                       obj.openid=res.data.openid;  
+                       obj.expires_in=Date.now()+res.data.expires_in;  
+                       //console.log(obj);
+                       wx.setStorageSync('user', obj);//存储openid  
+                   }  
+               });
+           }else {
+               console.log('获取用户登录态失败！' + res.errMsg)
+           }          
+       }  
+     }); 
+   } 
   },
   data:{
     optionNames:['A','B','C','D','E','F']
